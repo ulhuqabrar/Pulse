483685c1127da4b751a8635277d56a2be66f714e
ulhuqabrar <ulhuqabrar001.com>
2025-08-29 22:30:29 +0530

    Made walls logic and logic to avoid it

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@
 
 using namespace std;
 
+void maybeSpawnWall(vector<vector<char>>& grid, int N) {
+
+}
+
+
 struct Agent
 {
-    int r, c, dr, dc;
+    int r, c, dr, dc, new_r, new_c;
     char symbol;
 };
 
@@ -48,15 +53,17 @@ int main() {
 
     srand(time(0));
     int range = rand() % (maxAgents - minAgents + 1) + minAgents;
+
     string symbols= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                     "abcdefghijklmnopqrstuvwxyz";
 
-    cout << "Total number of agents: " << range ;
+    cout << "Total number of agents: " << range << endl;
 
-    cout << endl;
     cout << endl;
 
     vector<Agent> agents;
+
+
 
     Agent a;
     int r, c, dr, dc;
@@ -87,6 +94,27 @@ int main() {
         grid [agents[i].dr][agents[i].dc] = '*';
     }
 
+    int walls = (N * N) * (rand() % (10 - 5 + 1) + 10) / 100;
+
+    int wall_r, wall_c;
+
+    for (int k = 0; k < walls; k++) {
+
+        int maxwalls = 0.2 * N * N;
+        int wallcount = 0;
+        int chance = rand() % 100;
+        if (chance < 30 && wallcount < maxwalls){
+            do {
+                wall_r = rand() % N;
+                wall_c = rand() % N;
+            } while (grid[wall_r][wall_c] != '.');
+
+            grid[wall_r][wall_c] = '#';
+
+            wallcount++;
+        }
+    }
+
     bool allReached = false;
 
     while (!allReached) {
@@ -98,21 +126,34 @@ int main() {
                 int old_r = agents[i].r;
                 int old_c = agents[i].c;
 
-                if (agents[i].r < agents[i].dr) agents[i].r++;
-                else if (agents[i].r > agents[i].dr) agents[i].r--;
+                int new_r = old_r;
+                int new_c = old_c;
 
-                if (agents[i].c < agents[i].dc) agents[i].c++;
-                else if (agents[i].c > agents[i].dc) agents[i].c--;
+                if (old_r > agents[i].dr) new_r = old_r - 1;
+                else if (old_r < agents[i].dr) new_r = old_r + 1;
+
+                if (old_c > agents[i].dc) new_c = old_c - 1;
+                else if (old_c < agents[i].dc) new_c = old_c + 1;
+
+                if (grid[new_r][old_c] == '#') new_r = old_r;
+                if (grid[old_r][new_c] == '#') new_c = old_c;
 
                 grid[old_r][old_c] = '.';
-                grid[agents[i].r][agents[i].c] = agents[i].symbol;
+                agents[i].r = new_r;
+                agents[i].c = new_c;
+                grid[new_r][new_c] = agents[i].symbol;  // agent marker
 
-                // show movement
                 printGrid(grid, N);
-                this_thread::sleep_for(chrono::milliseconds(300));
+                int ms = 300;
+                this_thread::sleep_for(chrono::milliseconds(ms));
 
             }
         }
+        maybeSpawnWall(grid, N);
+
+        int MS = 1500;
+        printGrid(grid, N);
+        this_thread::sleep_for(chrono::milliseconds(MS));
     }
 
     for (int i = 0; i < N; i++) {
